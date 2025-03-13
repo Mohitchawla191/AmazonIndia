16ed7770aac744a9b1f5e4c3a6caaf2594fe6a38
sunny.chawla <sunny.chawla910@gmail.com>
2025-03-13 22:29:46 +0530

    HP is doing 1st edit

--- src/main/java/GitPractice/TestClass1.java
+++ src/main/java/GitPractice/TestClass1.java
@@ -2,6 +2,6 @@ package GitPractice;
 
 public class TestClass1 {
     public static void main(String[] args) {
-        System.out.println("HP is coding");
+        System.out.println("HP is coding well");
     }
 }
