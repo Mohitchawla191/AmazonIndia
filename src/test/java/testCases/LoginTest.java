81ad00c650b749d4982eea9af755054c24ee6c4f
mohitchawla191 <mohitchawla.engineer@gmail.com>
2025-03-15 14:56:23 +0530

    Mar15 commit

--- src/test/java/testCases/LoginTest.java
+++ src/test/java/testCases/LoginTest.java
@@ -1,6 +1,7 @@
 package testCases;
 
 import base.BaseClass;
+import listeners.RetryAnalyzer;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
@@ -9,17 +10,17 @@ import pageObject.*;
 import utils.DataProviderClass;
 
 public class LoginTest extends BaseClass {
-
+    static LandingPage landingPage;
     @Test(dataProvider = "validCredentials", dataProviderClass = DataProviderClass.class, priority = 1, groups = "Smoke", enabled = true)
     public static void validLoginTest(String userId, String password){
-        LandingPage landingPage = new LandingPage();
+        landingPage = new LandingPage();
 
         LoginPage loginPage= landingPage.initiateSignIn();
         HomePage homePage = loginPage.validLogin(userId,password);
     }
     @Test(dataProvider = "invalidCredentials", dataProviderClass = DataProviderClass.class, priority = 2, enabled = false)
     public static void invalidLoginTest(String userId, String password){
-        LandingPage landingPage = new LandingPage();
+        landingPage = new LandingPage();
 
         LoginPage loginPage= landingPage.initiateSignIn();
         HomePage homePage = loginPage.invalidLogin(userId,password);
